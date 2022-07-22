8bd0f05f43f2e6cf316624e4d41cc6173762e496
andres <andresclua@gmail.com>
2022-07-22 18:29:32 +0200

    2nd version

--- src/index.js
+++ src/index.js
@@ -5,9 +5,24 @@ class Page{
     }
     init(){ 
         const tabs = new Tabs({
-            tabActive : 'tab-2',
+            tabActive : 'tab-a-2',
             tabActiveClass:'b--tabs-a__bd__item--is-active',
             tabBodyActiveClass: 'b--tabs-a__hd__list-item__link--is-active',
+            tabTrigger : 'tf-ds-tab-to-open',
+            tabBody : 'tf-ds-tab-body',
+            onHide : () => {
+                console.log("hide tabs");
+            },
+            onShow : () =>{
+                console.log("show tab");
+            }
+        });
+
+
+        const tabs2 = new Tabs({
+            tabActive : 'tab-b-1',
+            tabActiveClass:'b--tabs-b__bd__item--is-active',
+            tabBodyActiveClass: 'b--tabs-b__hd__list-item__link--is-active',
             tabTrigger : 'tf-ds-tab-to-open',
             tabBody : 'tf-ds-tab-body',
             onHide : () => {
